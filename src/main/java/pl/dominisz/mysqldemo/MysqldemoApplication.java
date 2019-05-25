7f83ca5db86bfec40a448ad8b50e7572c573965b
dominisz <dominisz@gmail.com>
2019-05-25 13:54:35 +0200

    Initial commit

---
+++ src/main/java/pl/dominisz/mysqldemo/MysqldemoApplication.java
@@ -0,0 +1,13 @@
+package pl.dominisz.mysqldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqldemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysqldemoApplication.class, args);
+    }
+
+}
